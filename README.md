ab54e00cca6854e62476779441ffc55834d93045
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2023-11-03 21:15:35 +0300

    chore: init project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# bubble-dizzy-remake
+# Bubble Dizzy Redux
+
+## Remake of the Bubble Dizzy
+
+Bubble Dizzy Redux is intended to be a full remake of the original [Bubble Dizzy](https://yolkfolk.com/games/bubble-dizzy/), aiming to improve upon the game's presentation and features, while retaining its core mechanics and charm.
+
+## Made in the Godot Engine
+
+Bubble Dizzy Redux is built in the [Godot Engine](https://godotengine.org/). This allows us to produce a modern, reliable game that to port to a range of different platforms, including mobile phones and web.
---
+++ project-4-0/scripts/window_manager.gd
@@ -0,0 +1,41 @@
+extends Node
+
+const _windowedModeRatio : float = 0.75;
+const _maximumAspectRatio : float = 16.0 / 9.0;  # 16 : 9
+const _minimumAspectRatio : float =  4.0 / 3.0;  #  4 : 3
+
+var _inFullScreen : bool = false;
+
+func _ready() -> void:
+  _inFullScreen = true;
+  switchMode();
+
+func _process(_delta: float) -> void:
+  if Input.is_action_just_pressed("ui_toogle_fullscreen"):
+    switchMode();
+
+func switchMode() -> void:
+  _inFullScreen = !_inFullScreen;
+
+  var window = get_window();
+  var screenSize = DisplayServer.screen_get_size(window.current_screen);
+  window.mode = DisplayServer.WINDOW_MODE_FULLSCREEN if _inFullScreen else DisplayServer.WINDOW_MODE_WINDOWED;
+  window.size = _calculateWindowSize(_inFullScreen, screenSize);
+  window.position = DisplayServer.screen_get_position(window.current_screen) + _calculateWindowPosition(_inFullScreen, screenSize);
+
+func _calculateWindowPosition(fullScreen : bool, screenSize : Vector2i) -> Vector2i:
+  return (screenSize - _calculateWindowSize(fullScreen, screenSize)) / 2;
+
+func _calculateWindowSize(fullScreen : bool, screenSize : Vector2i) -> Vector2i:
+  var widthHeight = screenSize;
+
+  if (!fullScreen):
+    widthHeight *= _windowedModeRatio;
+    if (widthHeight.aspect() > _maximumAspectRatio):
+      widthHeight.x = widthHeight.y * _maximumAspectRatio;
+    if (widthHeight.aspect() < _minimumAspectRatio):
+      widthHeight.y = widthHeight.x / _minimumAspectRatio;
+    widthHeight.x = int(widthHeight.x) & ~1;
+    widthHeight.y = int(widthHeight.y) & ~1;
+
+  return widthHeight;
