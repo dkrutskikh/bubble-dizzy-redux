c01b3511da595df1fb0cb928ded4510761b7c371
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2025-06-12 07:28:52 +0300

    fix

--- hello.cpp
+++ hello.cpp
@@ -4,12 +4,14 @@
 
 using namespace std;
 
-int main() {
-  vector<string> msg{"Hello", "C++",     "World",
-                     "from",  "VS Code", "and the C++ extension!"};
+int main()
+{
+      vector<string> msg{
+        "Hello", "C++", "World", "from", "VS Code", "and the C++ extension!",
+    };
 
-  for (const string& word : msg) {
-    cout << word << " ";
-  }
-  cout << endl;
+    for ( const string & word : msg ) {
+        cout << word << " ";
+    }
+    cout << endl;
 }
--- tools/builder/check_code_format.sh
+++ tools/builder/check_code_format.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-###########################################################################
-#   fheroes2: https://github.com/ihhub/fheroes2                           #
-#   Copyright (C) 2021 - 2025                                             #
-#                                                                         #
-#   Copyright 2018 Google LLC                                             #
-#                                                                         #
-#   This program is free software; you can redistribute it and/or modify  #
-#   it under the terms of the GNU General Public License as published by  #
-#   the Free Software Foundation; either version 2 of the License, or     #
-#   (at your option) any later version.                                   #
-#                                                                         #
-#   This program is distributed in the hope that it will be useful,       #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-#   GNU General Public License for more details.                          #
-#                                                                         #
-#   You should have received a copy of the GNU General Public License     #
-#   along with this program; if not, write to the                         #
-#   Free Software Foundation, Inc.,                                       #
-#   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
-###########################################################################
-
 # Script to determine whether the source code in the Pull Request is formatted correctly.
 # Exits with a non-zero exit code if formatting is needed.
 #
@@ -29,7 +7,7 @@
 
 set -e -o pipefail
 
-FILES_TO_CHECK=$(git diff --name-only HEAD^ | (grep -E ".*\.(cpp|cc|c\+\+|cxx|c|h|hpp|java|js)$" || true) \
+FILES_TO_CHECK=$(git diff --name-only HEAD^ | (grep -E ".*\.(cpp|cc|c\+\+|cxx|c|h|hpp|inl|java|js)$" || true) \
                                             | (grep -v "^src/thirdparty/.*/.*" || true))
 
 if [ -z "$FILES_TO_CHECK" ]; then
