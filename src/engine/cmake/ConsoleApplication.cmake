ba9184a6d7031b2ed45e0f5d83c17461cbdc2345
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2025-07-15 00:13:00 +0300

    fixes after review

--- src/engine/cmake/ConsoleApplication.cmake
+++ src/engine/cmake/ConsoleApplication.cmake
@@ -1,7 +1,11 @@
-function(console_application BINARY_NAME ${SRC_LIST} ${HDR_LIST} ${INL_LIST} ${LIB_LIST})
+function(console_application BINARY_NAME SRC_LIST ${HDR_LIST} ${INL_LIST} ${LIB_LIST})
 
-add_definitions(${PREPROCESSOR_DEFINITIONS})
+if(DEFINED PREPROCESSOR_DEFINITIONS)
+  add_definitions(${PREPROCESSOR_DEFINITIONS})
+endif(DEFINED PREPROCESSOR_DEFINITIONS)
+
 source_group("Source Files" FILES ${SRC_LIST} ${INL_LIST})
+
 add_executable(${BINARY_NAME} ${SRC_LIST} ${HDR_LIST} ${INL_LIST})
 target_link_libraries(${BINARY_NAME} ${LIB_LIST})
 
