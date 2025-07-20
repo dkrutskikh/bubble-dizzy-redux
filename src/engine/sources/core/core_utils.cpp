7e3bda7f3031bd0b66f370011489d58f6baea32c
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2025-07-20 15:41:18 +0300

    feat: introduce utf8toWChar

---
+++ src/engine/sources/core/core_utils.cpp
@@ -0,0 +1,65 @@
+//
+// Copyright 2025 by Toyman Interactive
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "../../include/core.hpp"
+
+namespace toygine
+{
+
+    wchar_t * utf8toWChar( wchar_t * dest, size_t destSize, char const * src, size_t srcLen )
+    {
+        if ( dest == nullptr || destSize == 0 )
+            return nullptr;
+
+        wchar_t * destPointer = dest;
+        if ( srcLen > 0 && src != nullptr ) {
+            wchar_t const * unicodeEndPos = dest + ( destSize - 1 );
+            size_t srcIterator = 0;
+
+            while ( srcIterator < srcLen && destPointer < unicodeEndPos ) {
+                std::uint8_t symbol = src[srcIterator++];
+                if ( symbol <= 0x7F ) {
+                    *destPointer = symbol;
+                }
+                else {
+                    size_t charBytes = 0;
+                    while ( symbol & 0x80 ) {
+                        ++charBytes;
+                        symbol <<= 1;
+                    }
+
+                    wchar_t unicodeChar = symbol >> charBytes;
+                    while ( charBytes-- > 1 ) {
+                        unicodeChar <<= 6;
+                        unicodeChar |= src[srcIterator++] & 0x3F;
+                    }
+
+                    *destPointer = unicodeChar;
+                }
+                ++destPointer;
+            }
+        }
+
+        *destPointer = L'\0';
+        return dest;
+    }
+
+} // namespace toygine
